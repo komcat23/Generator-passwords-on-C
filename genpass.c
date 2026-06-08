20d457d49ab494a4f7c723a26eeba5590a29d6cd
KomCat23 <artemkurnasenko2010@gmail.com>
2026-06-08 22:10:26 +0300

    Add files via upload

--- genpass.c
+++ genpass.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//P.S Cntr+C for close the programm
+
 int main(){
 	printf("Generator passwords on C\n");
 	printf("^C for close the programm\n");
@@ -13,7 +15,7 @@ int main(){
         	length = 8;
         	printf("Invalid length, using default: 8\n");
     	}
-    	char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*+-";
+    	char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*+/-";
     	int size = sizeof(letters); 
     	srand(time(NULL));
     	char password[length + 1];
